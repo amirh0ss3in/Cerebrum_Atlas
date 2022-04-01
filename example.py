8fc530c127c4bdc5311714d0639ea433e5534cf8
amirh0ss3in <rezaeeamirhosein@gmail.com>
2022-04-01 19:11:46 +0430

    cleaning the directory

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 import numpy as np
-from utils import plot_coordinates
+from CA.utils import plot_coordinates
 
 MNI_coords = np.array([2, 4, 60])
 plot_coordinates(MNI_coords, input_coordinate = 'mni', colored = True)
